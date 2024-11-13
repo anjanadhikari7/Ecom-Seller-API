3fbf4762228c5f10148d9bd8fcb3ae4b2d059198
anjanadhikari7 <166963730+anjanadhikari7@users.noreply.github.com>
2024-11-13 19:36:09 +1100

    updated: envs

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 // Middlewares
 app.use(
   cors({
-    origin: "process.env.CLIENT_ROOT_URL",
+    origin: process.env.CLIENT_ROOT_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
